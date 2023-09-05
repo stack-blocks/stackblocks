196b62533898d9310be2c98c5a5a5315136305b9
stack-blocks <142496975+stack-blocks@users.noreply.github.com>
2023-09-05 14:09:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,388 @@
-const toggleButton = document.getElementById('nav-toggle');
-        const navLinks = document.getElementById('nav-links');
-
-        toggleButton.addEventListener('click', () => {
-            navLinks.classList.toggle('active');
+gsap.from(".navbar",{
+    delay:.6,
+    opacity:0,
+    duration:0.3,
+    
+})
+gsap.from(".title>h1",{
+    delay:.6,
+    opacity:0,
+    duration:0.3,
+    x:60,
+})
+gsap.from(".title>p",{
+    delay:.6,
+    opacity:0,
+    duration:0.3,
+    x:60,
+})
+gsap.from(".title>img",{
+    delay:.6,
+    opacity:0,
+    duration:0.3,
+    x:-60,
+})
+gsap.from(".fea-head",{
+    
+    opacity:0,
+    duration:1,
+    delay:0.1,
+    x:-60,
+    scrollTrigger:{
+        trigger:".line",
+        scroller:"body",
+        start:"top 71%",
+        x:100,
+       
+    }
+})
+gsap.from(".title-para3",{
+    
+    opacity:0,
+    duration:1,
+    delay:0.1,
+    x:-60,
+    scrollTrigger:{
+        trigger:".line",
+        scroller:"body",
+        start:"top 71%",
+        x:100,
+       
+    }
+})
+gsap.from(".card1",{
+    
+    opacity:0,
+    duration:1,
+    delay:0.1,
+    y:-60,
+    scrollTrigger:{
+        trigger:".line",
+        scroller:"body",
+        start:"top 71%",
+        x:100,
+       
+    }
+})
+gsap.from(".card2",{
+    
+    opacity:0,
+    duration:1,
+    delay:0.1,
+    y:-60,
+    scrollTrigger:{
+        trigger:".line",
+        scroller:"body",
+        start:"top 71%",
+        x:100,
+       
+    }
+})
+gsap.from(".card3",{
+    
+    opacity:0,
+    duration:1,
+    delay:0.1,
+    y:-60,
+    scrollTrigger:{
+        trigger:".sit",
+        scroller:"body",
+        start:"top 71%",
+        x:100,
+       
+    }
+})
+gsap.from(".card4",{
+    
+    opacity:0,
+    duration:1,
+    delay:0.1,
+    y:60,
+    scrollTrigger:{
+        trigger:".sit",
+        scroller:"body",
+        start:"top 71%",
+        x:100,
+       
+    }
+})
+gsap.from(".sit",{
+    
+    opacity:0,
+    duration:1,
+    delay:0.1,
+    x:60,
+    scrollTrigger:{
+        trigger:".title-para3",
+        scroller:"body",
+        start:"top 71%",
+        x:100,
+       
+    }
+})
+gsap.from(".progress",{
+    
+    opacity:0,
+    duration:1,
+    delay:0.1,
+    x:60,
+    scrollTrigger:{
+        trigger:".team-strength",
+        scroller:"body",
+        start:"top 71%",
+        x:100,
+       
+    }
+})
+gsap.from(".level",{
+    
+    opacity:0,
+    duration:1,
+    delay:0.1,
+    y:60,
+    scrollTrigger:{
+        trigger:".team-strength",
+        scroller:"body",
+        start:"top 71%",
+        x:100,
+       
+    }
+})
+gsap.from(".progress1",{
+    
+    opacity:0,
+    duration:1,
+    delay:0.1,
+    x:-60,
+    scrollTrigger:{
+        trigger:".team-strength",
+        scroller:"body",
+        start:"top 71%",
+        x:100,
+       
+    }
+})
+gsap.from(".level1",{
+    
+    opacity:0,
+    duration:1,
+    delay:0.1,
+    y:60,
+    scrollTrigger:{
+        trigger:".team-strength",
+        scroller:"body",
+        start:"top 71%",
+        x:100,
+       
+    }
+})
+gsap.from(".price-title",{
+    
+    opacity:0,
+    duration:1,
+    delay:0.1,
+    x:-60,
+    scrollTrigger:{
+        trigger:"#liney",
+        scroller:"body",
+        start:"top 71%",
+        x:100,
+        
+       
+    }
+})
+gsap.from(".price-para",{
+    
+    opacity:0,
+    duration:1,
+    delay:0.1,
+    x:-60,
+    scrollTrigger:{
+        trigger:".price-title",
+        scroller:"body",
+        start:"top 71%",
+        x:100,
+        
+       
+    }
+})
+gsap.from("#box1",{
+    
+    opacity:0,
+    duration:1,
+    delay:0.1,
+    y:60,
+    scrollTrigger:{
+        trigger:".price-para",
+        scroller:"body",
+        start:"top 71%",
+        x:100,
+        
+       
+    }
+})
+gsap.from(".text1",{
+    
+    opacity:0,
+    duration:1,
+    delay:0.1,
+    x:-60,
+    scrollTrigger:{
+        trigger:".swiper-pagination",
+        scroller:"body",
+        start:"top 71%",
+        x:100,
+        
+       
+    }
+})
+// gsap.from(".splide_wrapper",{
+    
+//     opacity:0,
+//     duration:1,
+//     delay:0.1,
+//     x:-60,
+//     scrollTrigger:{
+//         trigger:".text1",
+//         scroller:"body",
+//         start:"top 71%",
+//         x:100,
+//         markers: true
+       
+//     }
+// })
+gsap.from(".working",{
+    
+    opacity:0,
+    duration:1,
+    delay:0.1,
+    x:60,
+    scrollTrigger:{
+        trigger:".text1",
+        scroller:"body",
+        start:"top 71%",
+        x:100,
+        
+       
+    }
+})
+gsap.from(".titlemax>p",{
+    
+    opacity:0,
+    duration:1,
+    delay:0.1,
+    x:60,
+    scrollTrigger:{
+        trigger:".why-title",
+        scroller:"body",
+        start:"top 71%",
+        x:100,
+        
+       
+    }
+})
+gsap.from(".why-bull",{
+    
+    opacity:0,
+    duration:1,
+    delay:0.1,
+    x:-60,
+    scrollTrigger:{
+        trigger:".titlemax>p",
+        scroller:"body",
+        start:"top 71%",
+        x:100,
+        
+       
+    }
+})
+gsap.from(".why-bull2",{
+    
+    opacity:0,
+    duration:1,
+    delay:0.1,
+    x:60,
+    scrollTrigger:{
+        trigger:".titlemax>p",
+        scroller:"body",
+        start:"top 71%",
+        x:100,
+        
+       
+    }
+})
+gsap.from(".textboss5>p",{
+    
+    opacity:0,
+    duration:1,
+    delay:0.1,
+    x:60,
+    scrollTrigger:{
+        trigger:".team-title",
+        scroller:"body",
+        start:"top 71%",
+        x:100,
+        
+       
+    }
+})
+gsap.from(".owl-carousel",{
+    
+    opacity:0,
+    duration:1,
+    delay:0.1,
+    y:60,
+    scrollTrigger:{
+        trigger:".textboss5>p",
+        scroller:"body",
+        start:"top 71%",
+        x:100,
+        
+       
+    }
+})
+gsap.from(".cus-title",{
+    
+    opacity:0,
+    duration:1,
+    delay:0.1,
+    x:60,
+    scrollTrigger:{
+        trigger:".line3",
+        scroller:"body",
+        start:"top 71%",
+        x:100,
+        
+       
+    }
+})
+gsap.from(".curse>p",{
+    
+    opacity:0,
+    duration:1,
+    delay:0.1,
+    x:60,
+    scrollTrigger:{
+        trigger:".cus-title",
+        scroller:"body",
+        start:"top 71%",
+        x:100,
+        
+       
+    }
+})
+gsap.from("#custo",{
+    
+    opacity:0,
+    duration:1,
+    delay:0.1,
+    y:60,
+    scrollTrigger:{
+        trigger:".curse>p",
+        scroller:"body",
+        start:"top 71%",
+        x:100,
+        
+       
+    }
 })
